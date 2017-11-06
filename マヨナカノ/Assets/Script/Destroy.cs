27f8a1ae395b4ef0ca7d027a2b29d2393bdd8be3
賀川裕登 <0zv4964f2pj291k@gmail.com>
2017-11-06 15:43:30 +0900

    ・ライトの光が消えるようになった ＊条件文を発生条件になるように変更する必要がある

---
+++ マヨナカノ/Assets/Script/Destroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("DestroyWin", 1000f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ マヨナカノ/Assets/Script/Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float vector = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += new Vector3(0, 0, -1);
+	}
+}
