0fe8e6e8bcbaefe128942e8266c59486283a5d73
賀川裕登 <0zv4964f2pj291k@gmail.com>
2017-11-07 15:56:29 +0900

    ・不気味な音を流すスクリプトを作成 ＊エラー出ているので対処しないといけない

--- マヨナカノ/Assets/Script/Trick_EerieSound.cs
+++ マヨナカノ/Assets/Script/Trick_EerieSound.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Trick_EerieSound : MonoBehaviour {
 
-	// Use this for initialization
+    Vector3  PlayerPos;//主人公の現在いる位置を入れる変数
+    public AudioSource EerieSound;
+
+    
+
+	//Use this for initialization
 	void Start () {
-		
+        EerieSound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+      
+        PlayerPos = transform.position;
+
+        if(PlayerPos.x>30||PlayerPos.z>50)
+        {
+            Debug.Log("不気味な音流す");
+            EerieSound.PlayOneShot(EerieSound.clip);
+        }
 	}
 }
