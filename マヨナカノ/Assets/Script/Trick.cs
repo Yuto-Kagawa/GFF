a4ada82bb3f035b18fcd6d038a2998f765b2a4c5
賀川裕登 <0zv4964f2pj291k@gmail.com>
2017-11-17 04:05:39 +0900

    ・音と物体Xは戻した ・懐中電灯の光の再点灯ができない

--- マヨナカノ/Assets/Script/Trick.cs
+++ マヨナカノ/Assets/Script/Trick.cs
@@ -7,7 +7,6 @@ public class Trick : MonoBehaviour
 {
     Vector3 PlayerPos;              //主人公の現在の位置を入れる変数
     public AudioSource EerieSound;  //不気味な音
-    int SoundFagCount;              //音を流すためのカウント(1の時は流す、それ以外は流さない)
     bool X_Flag;                    //物体Xを移動させるかどうか
     public X_Move X;
     int FlashLightDeleteTime;       //懐中電灯の光を消してる時間
@@ -19,7 +18,8 @@ public class Trick : MonoBehaviour
     void Start ()
     {
         EerieSound = GetComponent<AudioSource>();//音を取得
-        SoundFagCount = 0;
+        
+        //SoundFagCount = 0;
         X_Flag=false;
         FlashLightDeleteTime = 0;
         GameOverFlag = false;
@@ -37,14 +37,14 @@ public class Trick : MonoBehaviour
         //不気味な音を流す
         if (other.gameObject.tag == "Trick_EerieSound")   //タグ"Trick_EerieSound"に当たれば
         {
-            //Debug.Log("不気味な音流す");
+            Debug.Log("不気味な音流す");
             EerieSound.PlayOneShot(EerieSound.clip);//不気味な音を再生
         }
         //仕掛け2
         //物体Xが前を通る
         else if (other.gameObject.tag=="Trick_X")//タグ"Trick_X"に当たれば
         { 
-            //Debug.Log("物体X移動開始");
+           // Debug.Log("物体X移動開始");
             X.XFlag = true;
         }
         //仕掛け3
--- マヨナカノ/Assets/Script/Trick_EerieSound.cs
+++ マヨナカノ/Assets/Script/Trick_EerieSound.cs
@@ -4,30 +4,19 @@ using UnityEngine;
 
 public class Trick_EerieSound : MonoBehaviour
 {
-    Vector3  PlayerPos;//主人公の現在の位置を入れる変数
     public AudioSource EerieSound;//不気味な音
-    int SoundFagCount;//音を流すためのカウント(1の時は流す、それ以外は流さない)
+    public bool SoundFlag;
 
 	void Start ()
     {
-        EerieSound = GetComponent<AudioSource>();//音を取得
-        SoundFagCount = 0;
+        SoundFlag = false;
 	}
 
 	void Update ()
-    {    
-        PlayerPos = transform.position;//変数PlayerPosに現在の位置を代入する
-
-        if (PlayerPos.x > 47 && PlayerPos.z > 50)//主人公の位置がX=30,Z=50なら
-        {
-            Debug.Log("不気味な音流す");
-            SoundFagCount += 1;//カウントを足す
-        }
-
-        if (SoundFagCount == 1)
+    {
+        if (SoundFlag == true)
         {
             EerieSound.PlayOneShot(EerieSound.clip);//不気味な音を再生
-            SoundFagCount += 1;//カウントを増やし以降は流さない
         }
-	}
+    }
 }
